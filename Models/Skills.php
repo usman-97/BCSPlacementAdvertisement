ce33fe0c02dfaf9c3d121f026c9e222ea46336ff
usman-97 <usmanshabir65@hotmail.com>
2021-01-16 12:14:58 +0000

    SkillsData.php added

--- Models/Skills.php
+++ Models/Skills.php
@@ -1,1 +1,18 @@
 <?php
+require_once ("Database.php");
+
+
+class Skills {
+    protected $_dbInstance, $_dbHandle;
+
+    public function __construct()
+    {
+        $this->_dbInstance = Database::getInstance();
+        $this->_dbHandle = $this->_dbInstance->getdbConnection();
+    }
+
+    public function listPlacementSkills()
+    {
+        $sqlQuery = "SELECT * ";
+    }
+}
