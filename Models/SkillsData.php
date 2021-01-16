21f10ac6303f844cd7134b5ca538efc2861dd8ed
usman-97 <usmanshabir65@hotmail.com>
2021-01-16 13:08:57 +0000

    updated

--- Models/SkillsData.php
+++ Models/SkillsData.php
@@ -9,6 +9,26 @@ class SkillsData {
         $this->_skillID = $dbRow['skillID'];
         $this->_skill = $dbRow['skill'];
         $this->_startLevel = $dbRow['startLevel'];
-        $this->_endLevel = $dbRow['skillID'];
+        $this->_endLevel = $dbRow['endLevel'];
+    }
+
+    public function getSkillID()
+    {
+        return $this->_skillID;
+    }
+
+    public function getSkill()
+    {
+        return $this->_skill;
+    }
+
+    public function getStartLevel()
+    {
+        return $this->_startLevel;
+    }
+
+    public function getEndLevel()
+    {
+        return $this->_endLevel;
     }
 }
