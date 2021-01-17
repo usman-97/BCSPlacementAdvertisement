cfb69969f1b5f82bc25ab439ff5b3dcf5c66ecd4
usman-97 <usmanshabir65@hotmail.com>
2021-01-17 12:12:18 +0000

    create placement

--- createPlacement.php
+++ createPlacement.php
@@ -1,12 +1,31 @@
 <?php
-require_once ("Models/Skills.php");
+require_once ("Models/PlacementDataSet.php");
 
 $view = new stdClass();
 $view->pageTitle = "Create Placement";
-$skills = new Skills();
+$placement = new PlacementDataSet();
 
 require_once ("logout.php");
 
-$view->getAllSkills = $skills->listPlacementSkills();
+// $view->getAllSkills = $skills->listPlacementSkills();
+
+if (isset($_POST['createPlacement']))
+{
+    if (!empty($_POST['description']) && !empty($_POST['benefits']) && !empty($_POST['salary']) &&
+        !empty($_POST['salaryPaid']) && !empty($_POST['startDate']) && !empty($_POST['endDate']))
+    {
+        $placement->createPlacement($_POST['description'], $_POST['benefits'], $_POST['salary'], $_POST['salaryPaid'], $_POST['startDate'], $_POST['endDate']);
+        header("location: myPlacements.php");
+    }
+    else
+    {
+        $view->error = "Please fill all fields";
+    }
+}
+
+if (isset($_POST['cancel']))
+{
+    header("location: index.php");
+}
 
 require_once ("Views/createPlacement.phtml");
