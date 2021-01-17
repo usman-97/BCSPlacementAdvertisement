9e6f5cad637a639322613d86e42dd2aff0216a24
usman-97 <usmanshabir65@hotmail.com>
2021-01-17 13:40:53 +0000

    Display all placements on My Placement page

--- myPlacements.php
+++ myPlacements.php
@@ -5,6 +5,8 @@ $view = new stdClass();
 $view->pageTitle = "My Placement Advertisements";
 $placements = new PlacementDataSet();
 
+require_once ("logout.php");
+
 $view->AllPlacements = $placements->getAllPlacements();
 
 require_once ("Views/myPlacements.phtml");
