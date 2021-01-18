02b0d83e2d73b790e83a66271dce4f025cc68229
usman-97 <usmanshabir65@hotmail.com>
2021-01-18 02:50:55 +0000

    PlacementSkills.php added

--- Models/Skills.php
+++ Models/Skills.php
@@ -2,22 +2,36 @@
 require_once ("Database.php");
 require_once ("SkillsData.php");
 
-
+/**
+ * Class Skills
+ */
 class Skills {
     protected $_dbInstance, $_dbHandle;
 
+    /**
+     * Skills constructor.
+     */
     public function __construct()
     {
+        // Create Instance of Database
         $this->_dbInstance = Database::getInstance();
         $this->_dbHandle = $this->_dbInstance->getdbConnection();
     }
 
+    /**
+     * Lists all placements and their details
+     * @return array
+     */
     public function listPlacementSkills()
     {
-        $sqlQuery = "SELECT * FROM skills";
+        // SQL query to get all skills and their details
+        $sqlQuery = "SELECT * FROM skills ORDER BY skill";
+        // Prepare PDO statement
         $statement = $this->_dbHandle->prepare($sqlQuery);
+        // Execute PDO statement
         $statement->execute();
 
+        // Array list where are skills and their details will be stored
         $dataSet = [];
         while ($row = $statement->fetch())
         {
@@ -28,7 +42,7 @@ class Skills {
     }
 
     /**
-     *
+     * Add skills to placement
      */
     public function addSkill($skillID, $placementID)
     {
--- myPlacements.php
+++ myPlacements.php
@@ -1,12 +1,21 @@
 <?php
 require_once ("Models/PlacementDataSet.php");
+require_once ("Models/Skills.php");
 
 $view = new stdClass();
 $view->pageTitle = "My Placement Advertisements";
 $placements = new PlacementDataSet();
+$skills = new Skills();
 
 require_once ("logout.php");
 
-$view->AllPlacements = $placements->getAllPlacements();
+$view->allPlacements = $placements->getAllPlacements();
+$view->allSkills = $skills->listPlacementSkills();
+
+if (isset($_POST['addSkill']))
+{
+    $skill = $_POST['skill'];
+    var_dump($skill[-1]);
+}
 
 require_once ("Views/myPlacements.phtml");
