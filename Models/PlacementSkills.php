4bd4c7b8e27b24e08bc2383db847b4cc78de542f
usman-97 <usmanshabir65@hotmail.com>
2021-01-18 03:09:30 +0000

    Add skills to placements

--- Models/PlacementSkills.php
+++ Models/PlacementSkills.php
@@ -1,1 +1,32 @@
 <?php
+require_once ("Models/Database.php");
+require_once ("Models/SkillsData.php");
+require_once ("Models/PlacementData.php");
+
+/**
+ * Class PlacementSkills
+ */
+class PlacementSkills {
+    protected $_dbInstance, $_dbHandle;
+
+    /**
+     * PlacementSkills constructor.
+     */
+    public function __construct()
+    {
+        // Creates Instance of Database class
+        $this->_dbInstance = Database::getInstance();
+        // Establish the connection to database
+        $this->_dbHandle = $this->_dbInstance->getdbConnection();
+    }
+
+    public function addPlacementSkills($placement, $skill, $level)
+    {
+        $sqlQuery = "INSERT INTO placement_skills VALUES (:placement, :skill, :skillLevel)";
+        $statement = $this->_dbHandle->prepare($sqlQuery);
+        $statement->bindParam(":placement", $placement, PDO::PARAM_INT);
+        $statement->bindParam(":skill", $skill, PDO::PARAM_INT);
+        $statement->bindParam(":skillLevel", $level, PDO::PARAM_INT);
+        $statement->execute();
+    }
+}
