8d61f5fdf61874b5004fab9711dc909ac72865ec
usman-97 <usmanshabir65@hotmail.com>
2021-01-19 17:08:32 +0000

    update

--- myPlacements.php
+++ myPlacements.php
@@ -8,7 +8,6 @@ $view->pageTitle = "My Placement Advertisements";
 $placements = new PlacementDataSet();
 $skills = new Skills();
 $placementSkill = new PlacementSkills();
-$view->page = 0;
 
 require_once ("logout.php");
 
@@ -21,7 +20,7 @@ if (isset($_POST['showSkills'])) {
         // Then display all placement required skills
         $view->allPlacementSkills = $placementSkill->listAllPlacementSkills($_POST['placement_id']);
         // Keep track of current page
-        $view->page = $_POST['pageTrack'];
+        $_SESSION['page'] = $_POST['pageTrack'];
     }
 }
 
@@ -35,68 +34,50 @@ if (isset($_POST['addSkill']))
         $placementSkill->addPlacementSkills($_POST['placement_id'], $skill[0], $skill[-1]);
     }
     // Keep the track of current page
-    $view->page = $_POST['pageTrack'];
+    $_SESSION['page'] = $_POST['pageTrack'];
     // header ("location: myPlacements.php");
 }
 
-/*if (!isset($_SESSION['page']))
+if (!isset($_SESSION['page']))
 {
-    $_SESSION['page'] = 0;
-}*/
-
-/*if (isset($_POST['prePage']))
-{
-    if ($_SESSION['page'] > 0)
+    if (isset($_POST['pageTrack']))
     {
-        $_SESSION['add'] -= 1;
-    }
-    // var_dump($placements->countPlacementID());
-}
-
-if (isset($_POST['nextPage']))
-{
-        /*if ($_SESSION['page'] <= $placements->countPlacementID() - 1)
-        {*/
-            // $_SESSION['page'] += 1;
-        // }
-    // var_dump($placements->countPlacementID());
-// }
-
-if (isset($_POST['prePage']))
-{
-    if ($view->page > 0)
-    {
-        $view->page -= 1;
-    }
-}
-
-if (isset($_POST['nextPage']))
-{
-    if ($view->page == 0)
-    {
-        $view->page += 1;
+        $_SESSION['page'] = $_POST['pageTrack'];
     }
     else
     {
-        if (isset($_POST['pageTrack']))
-        {
-            $view->page = $_POST['pageTrack'] + 2;
-        }
+        $_SESSION['page'] = 0;
     }
 }
+// var_dump($_SESSION['page']);
 
-var_dump($view->page);
-var_dump($_POST['pageTrack']);
-
-if ($view->page > 0)
+if (isset($_POST['prePage']))
 {
-    $view->allPlacements = $placements->getAllPlacements( $view->page,  $view->page);
+    if ($_SESSION['page'] > 0)
+    {
+        $_SESSION['page'] -= 1;
+    }
+    // var_dump($placements->countPlacementID());
+}
+
+if (isset($_POST['nextPage']))
+{
+    if ($_SESSION['page'] < ($placements->countPlacementID() - 1)) {
+        $_SESSION['page'] += 1;
+    }
+    //var_dump($placements->countPlacementID());
+}
+
+// var_dump( $_SESSION['page']);
+// var_dump( $_POST['pageTrack']);
+
+if ( $_SESSION['page'] ==  0)
+{
+    $view->allPlacements = $placements->getAllPlacements($_SESSION['page'], ($_SESSION['page'] + 1));
 }
 else
 {
-    if ($view->page == 0) {
-        $view->allPlacements = $placements->getAllPlacements($view->page, ($view->page + 1));
-    }
+    $view->allPlacements = $placements->getAllPlacements($_SESSION['page'],  $_SESSION['page']);
 }
 
 require_once ("Views/myPlacements.phtml");
