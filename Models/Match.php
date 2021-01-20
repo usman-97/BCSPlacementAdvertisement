f6dd267079e39027108a908b0dd3dc6ed0853bc1
usman-97 <usmanshabir65@hotmail.com>
2021-01-20 09:53:18 +0000

    Match.php added

--- Models/Match.php
+++ Models/Match.php
@@ -12,4 +12,9 @@ class Match {
     {
 
     }
+
+    public function checkLocationSector($address, $sector)
+    {
+        $sqlQuery = "SELECT userID FROM users, student WHERE user_type = 'Student' AND postal_address = :address AND users.userID = student.user_id AND sector = :studentSector";
+    }
 }
