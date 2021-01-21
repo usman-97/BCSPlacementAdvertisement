45c815a96457bca37c01f5795d039020197d96c4
usman-97 <usmanshabir65@hotmail.com>
2021-01-21 15:51:51 +0000

    update

--- login.php
+++ login.php
@@ -14,45 +14,46 @@ require_once ("logout.php");
 // If Sign in button is pressed
 if (isset($_POST['login']))
 {
-    // Check if email and password fields are not empty
-    if (!empty(trim($_POST['userEmail'])) && !empty(trim($_POST['pwd'])))
+    if (empty(trim($_POST['username'])))
     {
-        $user->setEmail($_POST['userEmail']); // Set email field for User
-        $user->setPassword($_POST['pwd']); // Set password field for User
+        // Check if email and password fields are not empty
+        if (!empty(trim($_POST['userEmail'])) && !empty(trim($_POST['pwd']))) {
+            $user->setEmail($_POST['userEmail']); // Set email field for User
+            $user->setPassword($_POST['pwd']); // Set password field for User
 
-        // Check if email exist or match in database
-        if ($user->checkEmail()) {
-            if ($_POST['reCaptcha'] == $_POST['storeReCaptcha']) {
-                // If user password match with password in database
-                if ($user->verifyUser()) {
-                    // session_start(); // start session
+            // Check if email exist or match in database
+            if ($user->checkEmail()) {
+                if ($_POST['reCaptcha'] == $_POST['storeReCaptcha']) {
+                    // If user password match with password in database
+                    if ($user->verifyUser()) {
+                        // session_start(); // start session
 
-                    $_SESSION['loggedIn'] = true; // start loggedIn session
-                    $_SESSION['username'] = $user->getFullName(); // get full name of user
-                    $_SESSION['userID'] = $user->getUserID(); // get user id from database
-                    $_SESSION['typeOfUser'] = $user->getUserType(); // get type of user
-                    $_SESSION['email'] = $user->getEmail(); // get type of user
-                    $_SESSION['phone'] = $user->getPhoneNumber(); // get type of user
-                    $_SESSION['address'] = $user->getAddress(); // get type of user
-                    $_SESSION['timeout'] = time(); // get the time when user logged in
+                        $_SESSION['loggedIn'] = true; // start loggedIn session
+                        $_SESSION['username'] = $user->getFullName(); // get full name of user
+                        $_SESSION['userID'] = $user->getUserID(); // get user id from database
+                        $_SESSION['typeOfUser'] = $user->getUserType(); // get type of user
+                        $_SESSION['email'] = $user->getEmail(); // get type of user
+                        $_SESSION['phone'] = $user->getPhoneNumber(); // get type of user
+                        $_SESSION['address'] = $user->getAddress(); // get type of user
+                        $_SESSION['timeout'] = time(); // get the time when user logged in
 
-                    header("location: index.php");
+                        header("location: index.php");
+                    } else {
+                        $view->error = "Invalid password";
+                    }
+                } else {
+                    $view->error = "Incorrect reCaptcha";
                 }
-                else {
-                    $view->error = "Invalid password";
-                }
+            } else {
+                $view->error = "Invalid email";
             }
-            else {
-                $view->error = "Incorrect reCaptcha";
-            }
-        }
-        else {
-            $view->error = "Invalid email";
+        } else {
+            $view->error = "Please fill type email/password";
         }
     }
     else
     {
-        $view->error = "Please fill type email/password";
+        die("Oops..lost connection to database...");
     }
 }
 
--- register.php
+++ register.php
@@ -8,68 +8,60 @@ $view->error = "";
 $view->typeOfUser = "";
 
 // If sign up button is pressed
-if (isset($_POST['register']))
-{
-    // If fields in form are not empty
-    if (!empty(trim($_POST['newFullName'])) && !empty(trim($_POST['email'])) && !empty(trim($_POST['newPwd'])) &&
-        !empty(trim($_POST['phone_number'])) && !empty(trim($_POST['address'])))
+if (isset($_POST['register'])) {
+    if (empty(trim($_POST['username'])))
     {
-        if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
-        {
-            if (strlen($_POST['newPwd']) >= 8) {
-                $user->setEmail($_POST['email']); // Sets email field for user
-                $user->setPassword($_POST['newPwd']); // Sets password field for user
-                $user->setUserType($_POST['userType']); // Sets userType field for user
-                // If user email doesn't exist in database
-                if (!$user->checkEmail()) {
-                    if ($_POST['newPwd'] == $_POST['confirmPassword']) {
-                        if ($_POST['userType'] == "Student") {
-                            if (preg_match('/ac.uk$/', $_POST['email']))
-                            {
+        // If fields in form are not empty
+        if (!empty(trim($_POST['newFullName'])) && !empty(trim($_POST['email'])) && !empty(trim($_POST['newPwd'])) &&
+            !empty(trim($_POST['phone_number'])) && !empty(trim($_POST['address']))) {
+            if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                if (strlen($_POST['newPwd']) >= 8) {
+                    $user->setEmail($_POST['email']); // Sets email field for user
+                    $user->setPassword($_POST['newPwd']); // Sets password field for user
+                    $user->setUserType($_POST['userType']); // Sets userType field for user
+                    // If user email doesn't exist in database
+                    if (!$user->checkEmail()) {
+                        if ($_POST['newPwd'] == $_POST['confirmPassword']) {
+                            if ($_POST['userType'] == "Student") {
+                                if (preg_match('/ac.uk$/', $_POST['email'])) {
+                                    // Then register new user
+                                    $user->register($_POST['newFullName'], $_POST['phone_number'], $_POST['address']);
+                                    $user->addStudent();
+                                    header("location: login.php");
+                                } else {
+                                    $view->error = "Please use your College/University email to register";
+                                }
+                            }
+                            if ($_POST['userType'] == "Employer") {
                                 // Then register new user
                                 $user->register($_POST['newFullName'], $_POST['phone_number'], $_POST['address']);
-                                $user->addStudent();
+                                $user->addEmployer();
                                 header("location: login.php");
                             }
-                            else
+                            /*else
                             {
-                                $view->error = "Please use your College/University email to register";
-                            }
+                                $view->error = "Please choose between Student or employer";
+                            }*/
+                            // echo "Created";
+                        } else {
+                            $view->error = "Password doesn't match";
                         }
-                        if ($_POST['userType'] == "Employer")
-                        {
-                            // Then register new user
-                            $user->register($_POST['newFullName'], $_POST['phone_number'], $_POST['address']);
-                            $user->addEmployer();
-                            header("location: login.php");
-                        }
-                        /*else
-                        {
-                            $view->error = "Please choose between Student or employer";
-                        }*/
-                        // echo "Created";
+                    } else {
+                        $view->error = "This email is already taken";
                     }
-                    else {
-                        $view->error = "Password doesn't match";
-                    }
+                } else {
+                    $view->error = "Your password should be greater than 8 characters";
                 }
-                else {
-                    $view->error = "This email is already taken";
-                }
+            } else {
+                $view->error = "Invalid Email";
             }
-            else
-            {
-                $view->error = "Your password should be greater than 8 characters";
-            }
-        }
-        else
-        {
-            $view->error = "Invalid Email";
+        } else {
+            $view->error = "Please fill all fields";
         }
     }
     else
     {
-        $view->error = "Please fill all fields";
+        die("Oops...You lost the connection with database");
     }
 }
 
